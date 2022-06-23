4a5c75e96a0791cc0aa930fd501a9e8fd5bf8a27
Purvesh Deora <purveshdeora70@gmail.com>
2022-06-23 14:38:55 +0530

    update

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
 import Footer from "./components/footer/Footer";
+import './App.css'
 
 function App() {
   return (
